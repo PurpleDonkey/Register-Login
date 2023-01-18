f2c7cdd75efec259d59470111286251ccd1e42fd
PurpleDonkey <samizine007@gmail.com>
2023-01-18 15:11:11 +0800

    New
    Signed-off-by: PurpleDonkey <samizine007@gmail.com>

---
+++ Header/Head.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Resume</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Work+Sans:400"
+    />
+    <link rel="stylesheet" type="text/css" href="Head.css" />
+  </head>
+  <body>
+    <header>
+      <div class="header">
+        <a href="#Home">
+          <img src="logo.png" class="logo" />
+        </a>
+      </div>
+      <nav>
+        <ul class="nlinks">
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#AboutMe">About Us</a></li>
+          <li><a href="#Interests">Sign in</a></li>
+          <li><a href="#Education">Sign up</a></li>
+        </ul>
+      </nav>
+    </header>
+    </body>
+</html>
+
